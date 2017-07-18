e8b50de8e43afd172f14caf2b32a17014673acb1
zhengkuang <zhengkuang2016@gmail.com>
2017-07-18 15:29:59 +0800

    StackInvert

---
+++ StackInvert.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class StackInvert {
+    public Stack invertStack(Stack<Integer> a){
+        Integer myint=getAndRemoveFirstElement(a);
+        if(myint==null){
+            return a;
+        }
+        else{
+            invertStack(a);
+            a.push(myint);
+            return a;
+        }
+    }
+
+    public Integer getAndRemoveFirstElement(Stack<Integer> a){
+        if(a.empty()) return null;
+        int element=a.pop();
+        Integer first=getAndRemoveFirstElement(a);
+        if(first==null){
+            return element;
+        }
+        else{
+            a.push(element);
+            return first;
+        }
+
+    }
+
+    public static void main (String args[]){
+        Stack<Integer> myStack=new Stack<>();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+        StackInvert si=new StackInvert();
+        Stack<Integer> a=si.invertStack(myStack);
+    }
+}
