91d392fa78b6f342004f1a3c0d0eaf20b10aae80
zhengkuang <zhengkuang2016@gmail.com>
2017-08-21 08:53:10 -0400

    Morris

--- MorrisTraversal.java
+++ MorrisTraversal.java
@@ -32,6 +32,85 @@ public class MorrisTraversal {
         }
     }
 
+    public static void morrisTraversalPre(Node head){
+        if(head==null) return;
+        Node cur1=head;
+        Node cur2=null;
+        while(cur1!=null){
+            cur2= cur1.left;
+            if (cur2 != null) {
+                while(cur2.right!=null&&cur2.right!=cur1){
+                    cur2= cur2.right;
+                }
+                if(cur2.right==null){
+                    cur2.right=cur1;
+                    System.out.print(cur1.value);
+                    cur1=cur1.left;
+                    continue;
+                }
+                else{
+                    cur2.right=null;
+                }
+            }
+            if(cur1.left==null){
+                System.out.print(cur1.value);
+            }
+            cur1=cur1.right;
+        }
+    }
+
+
+    public static void morrisTraversalPost(Node head){
+        if(head==null) return;
+        Node cur1=head;
+        Node cur2=null;
+        while(cur1!=null){
+            cur2= cur1.left;
+            if (cur2 != null) {
+                while(cur2.right!=null&&cur2.right!=cur1){
+                    cur2= cur2.right;
+                }
+                if(cur2.right==null){
+                    cur2.right=cur1;
+                    cur1=cur1.left;
+                    continue;
+                }
+                else{
+                    cur2.right=null;
+                    Node leaf=reverseRight(cur1.left);
+                    Node tmp=leaf;
+                    while(tmp!=null){
+                        System.out.print(tmp.value);
+                        tmp=tmp.right;
+                    }
+                    reverseRight(leaf);
+                }
+            }
+            cur1=cur1.right;
+        }
+        Node leaf=reverseRight(head);
+        Node tmp=leaf;
+        while(tmp!=null){
+            System.out.print(tmp.value);
+            tmp=tmp.right;
+        }
+        reverseRight(leaf);
+
+    }
+
+    public static Node reverseRight(Node root){
+        Node cur=root;
+        Node pre=null;
+        while(cur!=null){
+            Node tmp=cur.right;
+            cur.right=pre;
+            pre=cur;
+            cur=tmp;
+        }
+        return pre;
+    }
+
+
     public static void main(String args[]){
         Node node=new Node(1);
         node.left=new Node(2);
@@ -40,6 +119,8 @@ public class MorrisTraversal {
         node.left.right=new Node(5);
         node.right.left=new Node(6);
         node.right.right=new Node(7);
-        morrisTraversalIn(node);
+        morrisTraversalPost(node);
     }
+
+
 }
