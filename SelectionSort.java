3ad1228b3476ad6003a706ad0dd8883fb2417ea1
zhengkuang <zhengkuang2016@gmail.com>
2017-08-24 09:52:18 -0400

    SelectionSort

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String args[]){
+        int a[]={3,2,4,5,1,6,7,4,8};
+        selection(a);
+        System.out.print(Arrays.toString(a));
+    }
+
+    public static void selection(int []num){
+        for(int i=0;i<num.length;i++){
+            int minIndex=i;
+            for(int j=i+1;j<num.length;j++){
+                if(num[j]<num[i]){
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                swap(num,minIndex,i);
+            }
+        }
+    }
+
+    public static void swap(int[] num,int minIndex,int i){
+        int tmp=num[minIndex];
+        num[minIndex]=num[i];
+        num[i]=tmp;
+    }
+}
