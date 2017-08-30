74b0283707f4f28473a032560315c481942b8a39
zhengkuang <zhengkuang2016@gmail.com>
2017-08-30 14:12:34 -0400

    quickSort

--- QuickSort.java
+++ QuickSort.java
@@ -1,10 +1,9 @@
 import java.util.Arrays;
 
 public class QuickSort {
-    public static int devide(int[] array, int left,int right, int k){
+    public static int devide(int[] array, int left,int right){
         int i=left,j=right;
         int pivot=array[(left+right)/2];
-        int pivotindex=(left+right)/2;
         while(i<=j){
             while(array[i]<pivot){
                 i++;
@@ -13,15 +12,6 @@ public class QuickSort {
                 j--;
             }
             if(i<=j){
-                if(array[i]==pivot&&array[j]==pivot){
-                    pivotindex=pivotindex;
-                }
-                else if(array[i]==pivot&&array[j]!=pivot){
-                    pivotindex=j;
-                }
-                else if(array[i]!=pivot&&array[j]==pivot){
-                    pivotindex=i;
-                }
                 int tmp=array[i];
                 array[i]=array[j];
                 array[j]=tmp;
@@ -29,24 +19,22 @@ public class QuickSort {
                 j--;
             }
         }
-        System.out.println(Arrays.toString(array)+"  pivot: "+pivot+"  pivotindex: "+pivotindex+" left "+left+" right "+right);
-        return pivotindex;
+        return i;
     }
 
-    public static void quickSort(int[] arr,int left,int right,int k){
-        int index=devide(arr,left,right,k);
-        if(index+1>k){
-            quickSort(arr,left,index-1,k);
+    public static void quickSort(int[] arr,int left,int right){
+        int index=devide(arr,left,right);
+        if(left<index-1){
+            quickSort(arr,left,index-1);
         }
-        else if(index+1<k){
-            quickSort(arr,index+1,right,k);
+        if(right>index){
+            quickSort(arr,index,right);
         }
-        else System.out.println(arr[index]);
     }
 
     public static void main(String args[]){
         int a[]={1,2,3,7,10,7,8};
-        quickSort(a,0,a.length-1,7);
+        quickSort(a,0,a.length-1);
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]);
         }
